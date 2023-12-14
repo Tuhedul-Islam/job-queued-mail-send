859ff23dffb59a32ec469aca5bb91d68d26a1c3f
Tuhedul Islam <tuhedulislamh15@gmail.com>
2023-12-14 10:59:15 +0600

    Home Controller Adde to testing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::get('/', [HomeController::class,'index']);
 
 Route::get('email-test', function(){
     $details['email'] = 'tuhedulislamh15@gmail.com';
